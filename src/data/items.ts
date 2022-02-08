8902d001e0b9de683e175d2076b8a890a28c16d2
Angelica Lirio <angelicablirio@gmail.com>
2022-02-08 09:39:07 -0300

    Criação projeto 06

---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+  {date: new Date(2022, 1, 6), category: 'food', title: 'BurgerKing', value: 55.36},
+  {date: new Date(2022, 1, 9), category: 'food', title: 'Mercado', value: 105.42},
+  {date: new Date(2022, 1, 10), category: 'rent', title: 'Aluguel Apto', value: 2000},
+  {date: new Date(2022, 1, 5), category: 'salary', title: 'Salário', value: 5000},
+  {date: new Date(2022, 2, 5), category: 'salary', title: 'Salário', value: 5000}
+];
